6d1636fe536c3ec45219d13c475bbc946980c17b
Tejal-Page <71485773+Tejal-Page@users.noreply.github.com>
2020-12-16 12:09:46 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,11 +7,16 @@ var distance = 0;
 var database;
 
 var form, player, game;
-
+//CRREATED VAR FOR 4 CARS- THEY WILL BE SPRITES
+//CARS- WILL BE ARRAY STORING INFO ABOUT 4 CARS- IE X AND Y POSTION OF CAR
+//CAR COMES INTO PICTURE, ONLY AFTER WAIT STATE ENDS
 var cars, car1, car2, car3, car4;
 
 
 function setup(){
+  //CHANGES MADE IN THE CANVAS SIZE
+  //DISPLAYWDTH N DISPLAYHEIGHT- CAPTURES THE SCREEN SIZE OF YOUR SYSTEM
+  //ACCORDINGLY MAKE CHANGES IN FORM CLASS WHERE YOU HAVE DISPLAYED THE TITLE, GREETING ETC
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
   game = new Game();
