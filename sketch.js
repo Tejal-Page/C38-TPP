f511c1b8d65426d79ea22401485a27e26b8bd743
Tejal-Page <71485773+Tejal-Page@users.noreply.github.com>
2020-12-16 16:20:41 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,6 +17,10 @@ function setup(){
   //CHANGES MADE IN THE CANVAS SIZE
   //DISPLAYWDTH N DISPLAYHEIGHT- CAPTURES THE SCREEN SIZE OF YOUR SYSTEM
   //ACCORDINGLY MAKE CHANGES IN FORM CLASS WHERE YOU HAVE DISPLAYED THE TITLE, GREETING ETC
+  /*
+  windowwidth or window height takes the entire screen size..like if ur screen is 17 inch..it takes 17 inch..
+  displaywidth and display height takes the displaying window size...like if u resize ur chrome tab..this new size is takenfor canvas
+  */
   canvas = createCanvas(displayWidth - 20, displayHeight-30);
   database = firebase.database();
   game = new Game();
